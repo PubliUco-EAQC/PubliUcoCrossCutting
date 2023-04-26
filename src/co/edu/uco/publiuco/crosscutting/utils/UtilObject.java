aa502e2da802a2cea5763390aa56ea097f5b75b0
edyquintero <edyquintero882@gmail.com>
2023-04-26 15:50:10 -0500

    Clase viernes 21 de abril

--- src/co/edu/uco/publiuco/crosscutting/utils/UtilObject.java
+++ src/co/edu/uco/publiuco/crosscutting/utils/UtilObject.java
@@ -1,5 +1,8 @@
 package co.edu.uco.publiuco.crosscutting.utils;
 
-public class UtilObject {
-
+public interface UtilObject {
+	
+	static <O> boolean isNull(final O object) {
+		return object == null;
+	}
 }
