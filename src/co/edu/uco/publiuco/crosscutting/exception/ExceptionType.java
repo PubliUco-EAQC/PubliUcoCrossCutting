57aef0c878c2c97dbbf5ecdfbc150901f2a0de6e
edyquintero <edyquintero882@gmail.com>
2023-05-16 15:30:37 -0500

    Commiteando muchossss muchosss cambios importantes (avance/desatraso)

---
+++ src/co/edu/uco/publiuco/crosscutting/exception/ExceptionType.java
@@ -0,0 +1,7 @@
+package co.edu.uco.publiuco.crosscutting.exception;
+
+public enum ExceptionType {
+	
+	DATA, BUSSNES, API, DTO, ENTITY, CROSSCUTTING, GENERAL
+	
+}
--- src/co/edu/uco/publiuco/crosscutting/exceptions/ExceptionType.java
+++
@@ -1,7 +0,0 @@
-package co.edu.uco.publiuco.crosscutting.exceptions;
-
-public enum ExceptionType {
-	
-	DATA, BUSSNES, API, DTO, ENTITY, CROSSCUTTING, GENERAL
-	
-}
